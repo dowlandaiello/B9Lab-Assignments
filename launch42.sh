5b1bcc65bd214b9fb88438e3074c7661dfed213d
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-14 16:40:14 -0400

    Reorganized a few things.

--- launch42.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run this once, but it does not hurt to run it every time
-geth --datadir /r/B9Lab-Assignments/net42 init /r/B9Lab-Assignments/genesis42.json
-# Run this every time you start your Geth "42", and add flags here as you need
-geth --datadir /r/B9Lab-Assignments/net42 --networkid 42 console
---
+++ privatenet/launch42macos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run this once, but it does not hurt to run it every time
+geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 init ~/desktop/B9Lab-Assignments/privatenet/genesis42.json
+# Run this every time you start your Geth "42", and add flags here as you need
+geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 --networkid 42 console
