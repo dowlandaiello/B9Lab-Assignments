e97ac570766eaf6faa8662a8ae1b79086b64f7ca
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-27 11:44:19 -0700

    Added give() robust0b method.

--- contracts/robust.sol
+++ contracts/robust.sol
@@ -7,10 +7,17 @@ contract Robust0a {
         require(!alreadyPaid, "already paid");
         alreadyPaid = true;
 
-        require(msg.sender.call.value(address(this).balance / 2)());
+        require(msg.sender.call.value(address(this).balance / 2)(), "");
     }
 }
 
 contract Robust0b {
     bool alreadyPaid;
+
+    function give() public {
+        require(!alreadyPaid, "already paid");
+        alreadyPaid = true;
+
+        msg.sender.transfer(address(this).balance / 2);
+    }
 }
