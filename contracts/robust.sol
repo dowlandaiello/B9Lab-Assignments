7b6b75bdde55dcd260a3ac57d58fac16a87a54a9
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-27 11:41:42 -0700

    Backup.

--- contracts/robust.sol
+++ contracts/robust.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.4.24;
 
 contract Robust0a {
+    bool alreadyPaid;
 
+    function give() public {
+        require(!alreadyPaid, "already paid");
+        alreadyPaid = true;
+    }
 }
