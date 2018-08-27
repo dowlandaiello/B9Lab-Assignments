8971feacbc9b37caa8974a1cb2e6c7391f4a484d
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-27 10:33:53 -0700

    Fixed warnings.

--- contracts/ownedinheritance.sol
+++ contracts/ownedinheritance.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.4.24;
 contract Owned {
     address owner;
 
-    function Owned() public {
+    constructor() public {
         owner = msg.sender;
     }
 }
 
 contract Purse is Owned {
     function pay(address whom, uint value) public {
-        if (msg.sender != owner) revert();
-        if (!whom.send(value)) revert();
+        if (msg.sender != owner) revert("sender is not owner");
+        whom.transfer(value);
     }
 }
