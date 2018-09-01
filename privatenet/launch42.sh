c7034c32c9f97d4bf654f44b8240458c261d71b9
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-01 14:50:56 -0400

    Added window rpc.

--- privatenet/launch42.sh
+++ privatenet/launch42.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Run this once, but it does not hurt to run it every time
-geth --datadir /r/B9Lab-Assignments/privatenet/net42 init /r/B9Lab-Assignments/privatenet/genesis42.json
+geth --datadir /r/B9Lab-Assignments/privatenet/net42 init /r/B9Lab-Assignments/privatenet/genesis42.json --rpc --rpcport 8545 --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi "eth,net,web3" --gcmode archive console
 # Run this every time you start your Geth "42", and add flags here as you need
-geth --datadir /r/B9Lab-Assignments/privatenet/net42 --networkid 42 console
+geth --datadir /r/B9Lab-Assignments/privatenet/net42 --networkid 42 console --rpc --rpcport 8545 --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi "eth,net,web3" --gcmode archive console
