c9bd32417222fa7e0719e0999a690d3d23753824
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-01 13:28:37 -0400

    Added rpc support.

--- privatenet/launch42macos.sh
+++ privatenet/launch42macos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Run this once, but it does not hurt to run it every time
-geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 init ~/desktop/B9Lab-Assignments/privatenet/genesis42.json --rpc
+geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 init ~/desktop/B9Lab-Assignments/privatenet/genesis42.json --rpc --rpcport 8545 --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi "eth,net,web3" --gcmode archive console
 # Run this every time you start your Geth "42", and add flags here as you need
-geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 --networkid 42 console --rpc
+geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 --networkid 42 console --rpc --rpcport 8545 --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi "eth,net,web3" --gcmode archive console
