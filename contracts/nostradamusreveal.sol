0cfaabf8e3f3fc60d46c7cee6d1ce75638fac71b
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-01 13:13:12 -0400

    Backup.

--- contracts/nostradamusreveal.sol
+++ contracts/nostradamusreveal.sol
@@ -8,17 +8,16 @@ contract NostradamusReveal4 {
     constructor() public {
     }
 
-    function prophecise(bytes32 exact) public {
-        uint blockNumber = block.number;
-        bytes32 blockHash = blockhash(blockNumber);
+    function prophecise() public {
+        bytes32 exact = theWord();
         require(keccak256(abi.encodePacked(msg.sender, blockNumber, blockHash, block.timestamp, this)) == exact, "");
         prophets[msg.sender] = true;
         emit LogProphecised(exact);
     }
 
-    function theWord() public view returns(bytes32 exact, bytes32 exact2) {
+    function theWord() public view returns(bytes32 exact) {
         uint blockNumber = block.number; // Fetch current block number
         bytes32 blockHash = blockhash(block.number); // Fetch hash of current block
-        return (keccak256(abi.encodePacked(msg.sender, blockHash, blockNumber, block.timestamp, this)), keccak256(abi.encodePacked(msg.sender, blockNumber, blockHash, block.timestamp, this))); // Hash byte value of address+latest block hash+latest block number+latest block timestamp
+        return (keccak256(abi.encodePacked(address(this), blockNumber, blockHash, block.timestamp, watch_addr))); // Hash byte value of address+latest block hash+latest block number+latest block timestamp
     }
 }
--- privatenet/launch42macos.sh
+++ privatenet/launch42macos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Run this once, but it does not hurt to run it every time
-geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 init ~/desktop/B9Lab-Assignments/privatenet/genesis42.json
+geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 init ~/desktop/B9Lab-Assignments/privatenet/genesis42.json --rpc
 # Run this every time you start your Geth "42", and add flags here as you need
-geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 --networkid 42 console
+geth --datadir ~/desktop/B9Lab-Assignments/privatenet/net42 --networkid 42 console --rpc
