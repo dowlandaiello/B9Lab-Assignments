70e4896f15d27e01d8390db0ff51f15fb98a463c
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-02 12:04:54 -0400

    Ree deployed!

--- test_project/truffle.js
+++ test_project/truffle.js
@@ -1,9 +1,15 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "fragile rely lady dance mass square will idle design private glass drop";
+
 module.exports = {
-  networks : {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*"
+    networks: {
+        ropsten: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/0db77166d63046319c02a3b5f9517b99");
+            },
+            network_id: 3,
+            gas: 2900000
+        }   
     }
-  }
 };
