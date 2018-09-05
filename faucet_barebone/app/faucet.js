19a5c045d278b5c0426219aabb8882d347ccabf5
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-05 13:19:56 -0400

    Init contract, added leveldb to gitignore.

--- faucet_barebone/app/faucet.js
+++ faucet_barebone/app/faucet.js
@@ -6,6 +6,7 @@ if (typeof web3 !== 'undefined') {
     // set the provider you want from Web3.providers
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
+
 web3.eth.getCoinbase(function(err, coinbase) {
     if (err) {
         console.error(err);
---
+++ splitter/contracts/Splitter.sol
@@ -0,0 +1,6 @@
+pragma solidity^0.4.23; // Specify compiler version
+
+// Init splitter contract
+contract Splitter {
+    
+}
