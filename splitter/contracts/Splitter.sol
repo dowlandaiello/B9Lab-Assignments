c0819600c5e4e2fcda2f104a0ee4f9c778cc66f1
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-06 08:36:58 -0400

    Init, implement send ether method

--- splitter/contracts/Splitter.sol
+++ splitter/contracts/Splitter.sol
@@ -2,11 +2,16 @@ pragma solidity^0.4.23; // Specify compiler version
 
 // Init splitter contract
 contract Splitter {
-    constructor() public {
+    address[] splitAddresses;
 
+    function split(address[] destinationAddresses) public payable {
+        splitAddresses = destinationAddresses;
+
+        sendEth();
     }
 
-    function split() public payable {
-
+    function sendEth() internal {
+        splitAddresses[0].transfer(msg.value/2);
+        splitAddresses[1].transfer(msg.value/2);
     }
 }
