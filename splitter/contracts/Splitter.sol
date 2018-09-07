0dde128488034506910f328d1d6d00f740f15fd5
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-07 13:28:12 -0400

    Backup.

--- splitter/contracts/Splitter.sol
+++ splitter/contracts/Splitter.sol
@@ -11,7 +11,7 @@ contract Splitter {
     }
 
     function sendEth() internal {
-        for (uint x = 0; x < splitAddresses.length; x++) {
+        for (uint x = 0; x != splitAddresses.length; x++) {
             splitAddresses[x].transfer(msg.value/splitAddresses.length); // Send ether
         }
     }
