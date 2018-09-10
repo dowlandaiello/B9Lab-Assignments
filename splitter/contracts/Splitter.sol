1fc3b32f38a665d3a7436696bd621143c91f0136
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-10 15:22:20 -0400

    Added web3

--- splitter/contracts/Splitter.sol
+++ splitter/contracts/Splitter.sol
@@ -2,17 +2,21 @@ pragma solidity^0.4.24; // Specify compiler version
 
 // Init splitter contract
 contract Splitter {
-    address[] splitAddresses;
+    mapping (address => uint256) unspentSplits; // Store pending withdrawals
 
-    function split(address destinationAddress1, address destinationAddress2) public payable {
-        splitAddresses = [destinationAddress1, destinationAddress2]; // Get specified addresses
-
-        sendEth(); // Send ether
+    function split(address[] destinationAddresses) public payable {
+        for (uint x = 0; x != destinationAddresses.length; x++) {
+            unspentSplits[destinationAddresses[x]] = msg.value / destinationAddresses.length;
+        }
     }
 
-    function sendEth() internal {
-        for (uint x = 0; x != splitAddresses.length; x++) {
-            require(splitAddresses[x].send(msg.value/splitAddresses.length), "Transaction failed"); // Send ether
-        }
+    function withdraw() public {
+        sendEth(msg.sender); // Send ether
+    }
+
+    function sendEth(address destinationAddress) internal {
+        destinationAddress.transfer(unspentSplits[destinationAddress]); // Attempt to transfer to specified address
+
+        unspentSplits[destinationAddress] = 0; // Set address pending split to 0
     }
 }
