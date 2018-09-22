cd3186271109784f57073ff59a149a8c9428cb78
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-21 23:41:34 -0400

    Added a few events, renamed timekeeping mapping.

--- remittance/contracts/Remittance.sol
+++ remittance/contracts/Remittance.sol
@@ -1,24 +1,34 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.24; // Specify compiler version
 
 // Init remittance contract
 contract Remittance {
     mapping (address => uint256) balances; // Balance of each address
-    mapping (address => uint256) balanceLastUpdated; // Last time balance was updated
+    mapping (address => uint256) balanceMaturity; // Last time balance was updated
     mapping (address => bytes32) publicKeys; // Public key associated with each address
+
+    event deposited(address sender, bytes32 publicKey, uint amount); // Log deposit
+    event attemptedClaim(address claimant, address claimAddress, bytes32 publicKey, uint amount); // Log claim
+    event attemptedWithdrawal(address withdrawer, uint balanceMaturity, uint amount); // Log withdrawal
     
     function deposit(string privatekey1, string privatekey2) public payable {
         balances[msg.sender] = msg.value; // Set user balance
-        balanceLastUpdated[msg.sender] = block.number; // Set updated time
+        balanceMaturity[msg.sender] = block.number; // Set updated time
 
         publicKeys[msg.sender] = keccak256(abi.encodePacked(privatekey1, privatekey2)); // Generate public key
+
+        emit deposited(msg.sender, publicKeys[msg.sender], msg.value); // Send deposit event
     }
 
     function claim(address claimAddress, string privatekey1, string privatekey2) public payable {
+        emit attemptedClaim(msg.sender, claimAddress, publicKeys[claimAddress], balances[claimAddress]); // Send claim event
+
         require(keccak256(abi.encodePacked(privatekey1, privatekey2)) == publicKeys[claimAddress], "Invalid private keys."); // Check for matching privatekeys
         require(msg.sender != claimAddress, "Cannot claim own balance (request a withdrawal instead)."); // Check that claimant isn't issuer
     }
 
     function withdraw() public payable {
-        require((block.number - balanceLastUpdated[msg.sender]) > 60, "Balance is not yet eligible for withdrawal."); // Check balance is mature enough for a withdrawal
+        emit attemptedWithdrawal(msg.sender, balanceMaturity[msg.sender], balances[msg.sender]); // Send withdrawal event
+
+        require((block.number - balanceMaturity[msg.sender]) > 60, "Balance is not yet eligible for withdrawal."); // Check balance is mature enough for a withdrawal
     }
 }
