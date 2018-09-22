26ed36d4d8ceed03dddd4312567181a83ba08abe
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-21 23:26:04 -0400

    Added deposit, claim, withdraw methods.

--- remittance/contracts/Remittance.sol
+++ remittance/contracts/Remittance.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.4.24;
 
+// Init remittance contract
 contract Remittance {
-   
+    mapping (address => uint256) balances; // Balance of each address
+    mapping (address => uint256) balanceLastUpdated; // Last time balance was updated
+    mapping (address => bytes32) publicKeys; // Public key associated with each address
+    
+    function deposit(string privatekey1, string privatekey2) public payable {
+        balances[msg.sender] = msg.value; // Set user balance
+        balanceLastUpdated[msg.sender] = block.number; // Set updated time
+
+        publicKeys[msg.sender] = keccak256(abi.encodePacked(privatekey1, privatekey2)); // Generate public key
+    }
+
+    function claim(address claimAddress, string privatekey1, string privatekey2) public payable {
+        require(keccak256(abi.encodePacked(privatekey1, privatekey2)) == publicKeys[claimAddress], "Invalid private keys."); // Check for matching privatekeys
+        require(msg.sender != claimAddress, "Cannot claim own balance (request a withdrawal instead)."); // Check that claimant isn't issuer
+    }
+
+    function withdraw() public payable {
+        require((block.number - balanceLastUpdated[msg.sender]) > 60, "Balance is not yet eligible for withdrawal."); // Check balance is mature enough for a withdrawal
+    }
 }
