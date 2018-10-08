f0b33ec5fe722a8aaedd9e8dfe14f26775f4c174
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-08 15:13:35 -0400

    Backup.

---
+++ RockPaperScissors/App/ethpaperscissors/src/components/ethpaperscissors.vue
@@ -0,0 +1,18 @@
+<template>
+    <metamask/>
+</template>
+
+<script>
+    import Metamask from '@/components/metamask'
+
+    export default {
+        name: 'ethpaperscissors',
+        components: {
+            'metamask': Metamask
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
