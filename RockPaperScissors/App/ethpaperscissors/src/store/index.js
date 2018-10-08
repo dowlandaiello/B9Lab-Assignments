022bb4d61fd7aff6580a0c4347e83698b4c62c2a
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-08 15:28:00 -0400

    Added web3.

--- RockPaperScissors/App/ethpaperscissors/src/store/index.js
+++ RockPaperScissors/App/ethpaperscissors/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import state from './state';
+import { getWeb3 } from '../util/getWeb3';
 
 Vue.use(Vuex);
 
@@ -8,5 +9,15 @@ export default new Vuex.Store({
   strict: true,
   state,
   mutations: {},
-  actions: {},
+  actions: {
+    registerWeb3({ commit }) {
+      console.log('registerWeb3 Action being executed');
+      getWeb3.then((result) => {
+        console.log('committing result to registerWeb3Instance mutation');
+        commit('registerWeb3Instance', result);
+      }).catch((e) => {
+        console.log('error in action registerWeb3', e);
+      });
+    },
+  },
 });
