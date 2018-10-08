6a5250545824362a88b84de5bf1e2c39ff0d0bb9
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-08 17:17:55 -0400

    Backup.

--- RockPaperScissors/App/ethpaperscissors/src/util/getWeb3.js
+++ RockPaperScissors/App/ethpaperscissors/src/util/getWeb3.js
@@ -16,7 +16,7 @@ export const getWeb3 = new Promise(((resolve, reject) => {
   if (typeof web3js !== 'undefined') {
     const web3 = new Web3(web3js.currentProvider);
     resolve({
-      injectedWeb3: web3.isConnected(),
+      injectedWeb3: web3.eth.net.isListening(),
       web3() {
         return web3;
       },
@@ -25,41 +25,38 @@ export const getWeb3 = new Promise(((resolve, reject) => {
     // web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')) GANACHE FALLBACK
     reject(new Error('Unable to connect to Metamask'));
   }
-}))
-  .then(result => new Promise(((resolve, reject) => {
-    // Retrieve network ID
-    result.web3().version.getNetwork((err, networkId) => {
-      if (err) {
-        // If we can't find a networkId keep result the same and reject the promise
-        reject(new Error('Unable to retrieve network ID'));
-      } else {
-        // Assign the networkId property to our result and resolve promise
-        result = Object.assign({}, result, { networkId });
-        resolve(result);
-      }
-    });
-  })))
-  .then(result => new Promise(((resolve, reject) => {
-    // Retrieve coinbase
-    result.web3().eth.getCoinbase((err, coinbase) => {
-      if (err) {
-        reject(new Error('Unable to retrieve coinbase'));
-      } else {
-        result = Object.assign({}, result, { coinbase });
-        resolve(result);
-      }
-    });
-  })))
-  .then(result => new Promise(((resolve, reject) => {
-    // Retrieve balance for coinbase
-    result.web3().eth.getBalance(result.coinbase, (err, balance) => {
-      if (err) {
-        reject(new Error(`Unable to retrieve balance for address: ${result.coinbase}`));
-      } else {
-        result = Object.assign({}, result, { balance });
-        resolve(result);
-      }
-    });
-  })));
+})).then(result => new Promise(((resolve, reject) => {
+  // Retrieve network ID
+  result.web3.eth.net.getId()((err, networkId) => {
+    if (err) {
+      // If we can't find a networkId keep result the same and reject the promise
+      reject(new Error('Unable to retrieve network ID'));
+    } else {
+      // Assign the networkId property to our result and resolve promise
+      result = Object.assign({}, result, { networkId });
+      resolve(result);
+    }
+  });
+}))).then(result => new Promise(((resolve, reject) => {
+  // Retrieve coinbase
+  result.web3().eth.getCoinbase((err, coinbase) => {
+    if (err) {
+      reject(new Error('Unable to retrieve coinbase'));
+    } else {
+      result = Object.assign({}, result, { coinbase });
+      resolve(result);
+    }
+  });
+}))).then(result => new Promise(((resolve, reject) => {
+  // Retrieve balance for coinbase
+  result.web3().eth.getBalance(result.coinbase, (err, balance) => {
+    if (err) {
+      reject(new Error(`Unable to retrieve balance for address: ${result.coinbase}`));
+    } else {
+      result = Object.assign({}, result, { balance });
+      resolve(result);
+    }
+  });
+})));
 
 export default getWeb3;
