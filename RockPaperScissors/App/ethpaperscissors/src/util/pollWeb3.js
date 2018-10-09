04485b431d18ea86585abe4ba1be0edead2077f4
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-09 12:25:55 -0400

    Added web3 polling.

---
+++ RockPaperScissors/App/ethpaperscissors/src/util/pollWeb3.js
@@ -0,0 +1,42 @@
+import Web3 from 'web3';
+import { store } from '../store/';
+
+/* eslint no-param-reassign: 0 */
+/* eslint no-unused-vars: 0 */
+
+const pollWeb3 = (state) => {
+  const web3js = window.web3;
+
+  const web3 = new Web3(web3js.currentProvider);
+
+  setInterval(() => {
+    if (web3 && store.state.web3.web3Instance) {
+      if (web3.eth.coinbase !== store.state.web3.coinbase) {
+        const newCoinbase = web3.eth.coinbase;
+        web3.eth.getBalance(web3.eth.coinbase, (err, newBalance) => {
+          if (err) {
+            console.log(err);
+          } else {
+            store.dispatch('pollWeb3', {
+              coinbase: newCoinbase,
+              balance: parseInt(newBalance, 10),
+            });
+          }
+        });
+      } else {
+        web3.eth.getBalance(store.state.web3.coinbase, (err, polledBalance) => {
+          if (err) {
+            console.log(err);
+          } else if (parseInt(polledBalance, 10) !== store.state.web3.balance) {
+            store.dispatch('pollWeb3', {
+              coinbase: store.state.web3.coinbase,
+              balance: polledBalance,
+            });
+          }
+        });
+      }
+    }
+  }, 500);
+};
+
+export default pollWeb3;
