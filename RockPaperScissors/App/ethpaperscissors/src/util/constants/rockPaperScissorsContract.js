6803d65feadaa720d41a765485255d969df8abec
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-09 19:53:26 -0400

    Deployed, updated app contract const.

--- RockPaperScissors/App/ethpaperscissors/src/util/constants/rockPaperScissorsContract.js
+++ RockPaperScissors/App/ethpaperscissors/src/util/constants/rockPaperScissorsContract.js
@@ -1,17 +1,19 @@
-const address = '0x6691c0be762284C4831146b55f929Bd61895eA86';
+const address = '0x5ef51758449f9151b1bd06d4EAbBBA3dD0F7dEd9';
 
 const ABI = [{
   constant: false, inputs: [{ name: '_inviteCode', type: 'bytes32' }], name: 'bet', outputs: [], payable: true, stateMutability: 'payable', type: 'function',
 }, {
   constant: false, inputs: [{ name: '_inviteCode', type: 'bytes32' }], name: 'claimBet', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
 }, {
-  constant: false, inputs: [{ name: '_inviteCode', type: 'bytes32' }, { name: '_move', type: 'uint256' }], name: 'move', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
-}, {
   constant: false, inputs: [], name: 'newGame', outputs: [{ name: '_inviteCode', type: 'bytes32' }], payable: false, stateMutability: 'nonpayable', type: 'function',
 }, {
   constant: true, inputs: [{ name: '_inviteCode', type: 'bytes32' }, { name: '_address', type: 'address' }], name: 'winCount', outputs: [{ name: '_winCount', type: 'uint256' }], payable: false, stateMutability: 'view', type: 'function',
 }, {
+  constant: false, inputs: [{ name: '_inviteCode', type: 'bytes32' }, { name: '_encryptedMove', type: 'bytes32' }], name: 'commitMove', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
+}, {
   constant: false, inputs: [{ name: '_inviteCode', type: 'bytes32' }], name: 'joinGame', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
+}, {
+  constant: false, inputs: [{ name: '_inviteCode', type: 'bytes32' }, { name: '_move', type: 'uint256' }], name: 'revealMove', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
 }, {
   constant: true, inputs: [{ name: '', type: 'bytes32' }], name: 'Games', outputs: [{ name: 'Initialized', type: 'bool' }, { name: 'InviteCode', type: 'bytes32' }, { name: 'GameFinished', type: 'bool' }, { name: 'Block', type: 'uint256' }, { name: 'RoundsPlayed', type: 'uint256' }, { name: 'Winner', type: 'address' }], payable: false, stateMutability: 'view', type: 'function',
 }, {
