df95f20fb046f12282b8aa91165da328c9e9cee3
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-14 13:24:01 -0400

    Remvoed dist from gitignore.

---
+++ RockPaperScissors/App/ethpaperscissors/dist/precache-manifest.8296df525861e7aae44201c07e142408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0d38a11d2038f012eb4c",
+    "url": "/js/chunk-vendors.5e0ecb57.js"
+  },
+  {
+    "revision": "78adab367ea784d91d60",
+    "url": "/js/app.76a4123b.js"
+  },
+  {
+    "revision": "5ec49e6dccb2ca4932e9",
+    "url": "/js/about.07b90307.js"
+  },
+  {
+    "revision": "82567d36be4d4e56fcb52d8712eba163",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78adab367ea784d91d60",
+    "url": "/css/app.e688a4d8.css"
+  }
+];
