7a4b5187aea8bf907f796d14b03e4b39dd753248
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-18 14:30:11 -0400

    Deployed react.

--- React/truffle.js
+++ React/truffle.js
@@ -1,4 +1,24 @@
+const Web3 = require("web3");
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "fragile rely lady dance mass square will idle design private glass drop";
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/0db77166d63046319c02a3b5f9517b99");
+      },
+      network_id: 3,
+      gas: 7000000
+    },
+    rinkeby : {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/0db77166d63046319c02a3b5f9517b99");
+      },
+      network_id: 4,
+      gas: 7000000
+    }
+  }
 };
