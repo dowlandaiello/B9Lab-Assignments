74df86ffc8c2b7f1afaf8e18ea1f9b42e77cbf68
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-18 16:28:58 -0400

    React support.

--- React/truffle.js
+++ React/truffle.js
@@ -13,12 +13,17 @@ module.exports = {
       network_id: 3,
       gas: 7000000
     },
-    rinkeby : {
+    rinkeby: {
       provider: function () {
         return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/0db77166d63046319c02a3b5f9517b99");
       },
       network_id: 4,
       gas: 7000000
+    },
+    private: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
     }
   }
 };
